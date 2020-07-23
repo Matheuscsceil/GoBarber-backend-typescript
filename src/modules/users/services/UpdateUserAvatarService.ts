f1dea029ed3576a65207217df26739d0f0470376
Matheuscsceil <math.cs.ceil@gmail.com>
2020-07-23 10:35:47 -0300

    Configuração e criação de testes com jest

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import {injectable,inject} from 'tsyringe';
 
 import AppError from '@shared/errors/AppError'
 import IUsersRepository from '../repositories/IUserRepository'
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 
 import User from '../infra/typeorm/entities/User'
 
@@ -17,7 +18,10 @@ interface IRequest{
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ){}
 
   public async execute ({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -28,16 +32,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-
-      const userAvatarFileExistes = await fs.promises.stat(userAvatarFilePath)
-
-      if (userAvatarFileExistes) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await this.storageProvider.deleteFile(user.avatar)
     }
 
-    user.avatar = avatarFilename
+    const filename = await this.storageProvider.saveFile(avatarFilename)
+
+    user.avatar = filename
 
     await this.usersRepository.save(user)
 
