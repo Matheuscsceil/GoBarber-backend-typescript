e62d9409e4edf5e76f122fb3ea3e518458a3b242
Matheuscsceil <math.cs.ceil@gmail.com>
2020-08-20 14:01:31 -0300

    TDD e update profile

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import appointmentRouter from '@modules/appointments/infra/http/routes/appointme
 import usersRoutes from '@modules/users/infra/http/routes/users.routes'
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes'
 import passwordRoutes from '@modules/users/infra/http/routes/password.routes'
+import profileRoutes from '@modules/users/infra/http/routes/profile.routes'
 
 const routes = Router()
 
@@ -11,5 +12,6 @@ routes.use('/appointments', appointmentRouter)
 routes.use('/users', usersRoutes)
 routes.use('/sessions', sessionsRoutes)
 routes.use('/password', passwordRoutes)
+routes.use('/profile',profileRoutes)
 
 export default routes
