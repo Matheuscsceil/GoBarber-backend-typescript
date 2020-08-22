88f8768882a31eeb03c90d5b9acae772886458dd
Matheuscsceil <math.cs.ceil@gmail.com>
2020-08-22 09:27:39 -0300

    Listagem de agendamentos, dias e horarios disponiveis

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,56 @@
+import {injectable,inject} from 'tsyringe';
+import {getHours, isAfter} from 'date-fns'
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepopsitory'
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject('IAppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+  ){}
+
+  public async execute ({ provider_id, day, year, month }: IRequest): Promise<IResponse> {
+    const appoitments = await this.appointmentsRepository.findAllInDayFromProvider({
+      provider_id, day, year, month
+    })
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      {length: 10},
+      (value,index) => index + hourStart
+    )
+
+    const currentDate = new Date(Date.now());
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appoitments.find(appoitment =>
+        getHours(appoitment.date) === hour
+      )
+
+      const compareDate = new Date(year, month - 1, day, hour);
+
+      return {
+        hour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate)
+      }
+    })
+
+    return availability
+  }
+}
+
+export default ListProviderDayAvailabilityService
